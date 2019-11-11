d9af74620e5638cdac21a16b951d54cf6e65aa8a
Geod11 <danilescugeorge@yahoo.com>
2019-11-11 17:51:21 +0200

    FizzBuzz

---
+++ ConsoleApp6/ConsoleApp6/FizzBuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class FizzBuzz
+    {
+        //Numerele de la 1 la 100. Daca este multiplu de 3 sa se afiseze Fizz
+        //Daca este multiplu de 5 sa se afiseze Buzz
+        //Daca este multiplu si de 3 si de 5 sa se afiseze FizzBuzz
+        public static void FizzBuzzMethod()
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz ");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz ");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz ");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} ");
+                }
+            }
+        }
+
+    }
+}
--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -10,42 +10,16 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
+            FizzBuzz.FizzBuzzMethod();
+
             Piramid.Piramida();
 
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine(Exercise23.Excercise23());
             }
-            //Numerele de la 1 la 100. Daca este multiplu de 3 sa se afiseze Fizz
-            //Daca este multiplu de 5 sa se afiseze Buzz
-            //Daca este multiplu si de 3 si de 5 sa se afiseze FizzBuzz
 
-            //for (int i = 1; i < 101; i++)
-            //{
-            //    if (i % 3 == 0 && i % 5 == 0)
-            //    {
-            //        Console.WriteLine("FizzBuzz ");
-            //    }
-
-            //    else if (i % 5 == 0)
-            //    {
-            //        Console.WriteLine("Buzz ");
-            //    }
-
-            //    else if (i % 3 == 0)
-            //    {
-            //        Console.WriteLine("Fizz ");                    
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine($"{i} ");
-            //    }                                
-            //}
-
-            //Un program care cere un cuvant si inverseaza prima cu ultima litera
-            //definim cuvant un string care are min 1 char diferit de spatiu 
-
-
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
         
