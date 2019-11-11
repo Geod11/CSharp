875c2327818acc5d1034fd15e885e80b7c5f97dd
Geod11 <danilescugeorge@yahoo.com>
2019-11-11 20:14:35 +0200

    Calculator, exercise 4

---
+++ ConsoleApp6/ConsoleApp6/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Calculator
+    {
+        public static void CalculatorMethod()
+        {
+            Console.Write("Please type in the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Please type in the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            int add = num1 + num2;
+            Console.WriteLine("The result of " + num1 + " + " + num2 + " = " + add);
+            int subtract = num1 - num2;
+            Console.WriteLine("The result of {0} - {1} = {2}", num1, num2, subtract);
+            int multiply = num1 * num2;
+            Console.WriteLine($"The result of {num1} x {num2} = {multiply}");
+            double divide = Convert.ToDouble(num1) / Convert.ToDouble(num2);           
+            if (num1 == 0 || num2 == 0 || (num1 == 0 && num2 == 0))
+            {
+                Console.WriteLine("Sorry but division by 0 is not possible");
+            }
+            else
+            {
+                int moduloResult = num1 % num2;
+                Console.WriteLine($"The result of {num1} / {num2} = {divide}");
+                Console.WriteLine($"The result of {num1} % {num2} = {moduloResult}");
+            }          
+        }
+    }
+}
--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -13,15 +13,18 @@ namespace ConsoleApp6
             // 1. Print the numbers from 1 to 100 with the following conditions:
             // - if the number is a multiple of 3 "Fizz" should be displayed instead;
             // - if the number is a multiple of 5 "Buzz" should be displayed instead;
-            // - if the number is a multiple of both 3 and 5 "FizzBuzz" should be displayed instead;
+            // - if the number is a multiple of both 3 and 5 "FizzBuzz" should be displayed instead.
             ////FizzBuzz.FizzBuzzMethod(); 
 
-            // 2. Display a piramid made from a character provided by the user with the height desired by the user as well;
+            // 2. Display a piramid made from a character provided by the user with the height desired by the user as well.
             ////Piramid.Piramida();
 
-                        
-                FirstLastString.FirstLastMethod();
-            
+            // 3. Ask for a string input from the user and swap the first and last letter/character of the first word.       
+            ////FirstLastString.FirstLastMethod();
+
+            // 4. Create a calculator that will take 2 numbers from the user and add, subtract, multiply, divide and calculate modulo.
+            Calculator.CalculatorMethod();
+
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
